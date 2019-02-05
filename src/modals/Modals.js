4e2e8f178a4044fa1c40691cfd9b2e5bd7382e66
jasonrfeliz <jason@meetmycampus.com>
2019-02-05 16:04:33 -0500

    started on adding modal feature for letting employers update a job post

---
+++ src/modals/Modals.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+import './Modals.scss'
+import UpdateJobPostModal from './UpdateJobPostModal'
+
+
+ const Modal = props => {
+   
+ }
+
+ export default Modal
